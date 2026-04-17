771659c3f8a0a3e0f2ebc7721f5187f74acd02d9
SergWebAcceptance <serg.web@acceptance.ltd>
2026-04-17 14:38:49 +0300

    Add metadata for About and How to Exchange pages, including titles and descriptions for improved SEO and social sharing. Import Metadata from Next.js to structure the data.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,9 +1,21 @@
+import { Metadata } from 'next';
+
 import {
   AboutHeroSection,
   AboutMissionSection,
   AboutProfessionalsSection,
 } from './components';
 import styles from './page.module.scss';
+
+export const metadata: Metadata = {
+  title: 'About Crypto Exchange | Bitvera',
+  description: 'Meet Bitvera crypto exchange for BTC and ETH. Learn how we built a fast, secure, and user-friendly platform for seamless transactions.',
+  openGraph: {
+    title: 'About Crypto Exchange | Bitvera',
+    description: 'Meet Bitvera crypto exchange for BTC and ETH. Learn how we built a fast, secure, and user-friendly platform for seamless transactions.',
+    //images: 'https://bitvera.com/images/meta.png',
+  },
+};
 
 export default function AboutPage() {
   return (
--- src/app/[locale]/how-to-exchange/page.tsx
+++ src/app/[locale]/how-to-exchange/page.tsx
@@ -1,9 +1,22 @@
+import { Metadata } from 'next';
+
 import {
   HowToExchangeHeroSection,
   HowToExchangeProcessSection,
   HowToExchangeWalletSection,
 } from './components';
 import styles from './page.module.scss';
+
+
+export const metadata: Metadata = {
+  title: 'How to Exchange Cryptocurrency | Bitvera',
+  description: 'Start exchanging BTC and ETH with Bitvera. Learn the step-by-step process, from registration and verification to wallet setup and secure transactions.',
+  openGraph: {
+    title: 'How to Exchange Cryptocurrency | Bitvera',
+    description: 'Start exchanging BTC and ETH with Bitvera. Learn the step-by-step process, from registration and verification to wallet setup and secure transactions.',
+    //images: 'https://bitvera.com/images/meta.png',
+  },
+};
 
 export default function HowToExchangePage() {
   return (
